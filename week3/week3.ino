3021b5bccf306af05d9923ea6ed1e128466dd43e
hyomin <andy6503@naver.com>
2020-11-23 12:05:41 +0900

    귀차늠

---
+++ week3/week3.ino
@@ -0,0 +1,17 @@
+int ledPin = 11; // ledPin을 7번으로 정의
+
+void setup() {
+  pinMode(ledPin, OUTPUT);    //ledPin을 출력으로 설정
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); //LED ON
+  delay(3000);                //1초 대기
+  digitalWrite(ledPin, LOW);  //LED OFF
+  delay(3000);                //1초 대기
+  digitalWrite(ledPin, LOW);  //LED OFF
+  delay(3000);                //1초 대기
+  digitalWrite(ledPin, LOW);  //LED OFF
+  delay(3000);   
+
+}
